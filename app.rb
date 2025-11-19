b73a37b3d5ec232899e28ac2508c585430a8458d
VidarKarman <vidar.karman@elev.ga.ntig.se>
2025-11-19 16:23:33 +0100

    lektion v 47 crd klart u nästa lektion

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ require 'sinatra/reloader'
 def db
   return @db if @db
 
-  @db = SQLite3::Database.new(DB_PATH)
+  @db = SQLite3::Database.new("db/todos.db")
   @db.results_as_hash = true
 
   return @db
@@ -18,7 +18,20 @@ end
 
 # Routen /
 get '/' do
-    slim(:index)
+  
+  @todo = db.execute('SELECT * FROM todos where state = 0')
+  @completed = db.execute('SELECT * FROM todos where state = 1')
+  slim(:index)
 end
-
-
+# Routen new, lägger till i databasen från ett formulär
+post '/new' do
+  new_task = params[:new_task]
+  new_description = params[:new_description]
+  db.execute('INSERT INTO todos (name, description, state) VALUES (?, ?, ?)', [new_task, new_description, 0])
+  redirect('/')
+end
+post '/delete/:id' do
+  id = params[:id]
+  db.execute('DELETE FROM todos where id=?', [id])
+  redirect('/')
+end
